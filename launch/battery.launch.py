86a8bd15e697c457726d537fca9a62660d3cf4ee
Alf-Olsen <123420311+Alf-Olsen@users.noreply.github.com>
2025-11-20 21:01:54 +0100

    Update parameters format in battery launch file

--- launch/battery.launch.py
+++ launch/battery.launch.py
@@ -7,12 +7,12 @@ def generate_launch_description():
             package="amr_sweeper_battery",
             executable="amr_sweeper_battery_node",
             name="amr_sweeper_battery",
-            parameters={
+            parameters=[{
                 "can_interface": "can0",
                 "timer_period": 1.0,
                 "priority": 0x18,
                 "bms_address": 0x01,
                 "pc_address": 0x40,
-            },
+            }],
         )
     ])
